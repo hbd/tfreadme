5ce60bed949e8a0c17e264eaefe6205b998ec558
Zachary A. Keener <zakkeener@gmail.com>
2019-03-09 13:22:36 -0500

    Add example tf files

---
+++ example/o.tf
@@ -0,0 +1,10 @@
+output "pickle_rick" {
+  value       = "It's Pickle Rick!"
+  description = "Solenya."
+  sensitive   = true
+}
+
+output "morty" {
+  value       = "Morty"
+  description = "Side kick."
+}
---
+++ example/variables.tf
@@ -0,0 +1,10 @@
+variable "enable_pickle_rick" {
+  description = "Enable the Pickle Rick."
+  type        = "string"
+  default     = "0"
+}
+
+variable "morty_id" {
+  description = "ID of Morty."
+  type        = "string"
+}
